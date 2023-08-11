c90cd3dc2134bfcd07fcdc805c80013dd06b615a
ReddeSpectre <103154892+ReddeSpectre@users.noreply.github.com>
2023-08-11 13:05:32 -0400

    Update virtualagent.py

--- virtualagent.py
+++ virtualagent.py
@@ -3,7 +3,6 @@ import random
 import pandas as pd 
 from sklearn.metrics import accuracy_score
 import string
-import matplotlib.pyplot as plt
 import openai
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
