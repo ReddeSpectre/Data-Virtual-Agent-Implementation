2caf1fc869915a95b19c599cab98f9530865e2c4
ReddeSpectre <103154892+ReddeSpectre@users.noreply.github.com>
2023-08-11 15:19:17 -0400

    Update virtualagent.py

--- virtualagent.py
+++ virtualagent.py
@@ -74,7 +74,7 @@ test_accuracy.append(test_acc)
 st.write("The results of the completed Train/Test split are as follows:")
 chart_data = {'train accuracy': train_accuracy, 'test accuracy': test_accuracy}
 chart_df = pd.DataFrame(data = chart_data)
-st.bar_chart(data = chart_data, x = None, y = None, width = 0, height = 0, use_container_width = True)
+st.bar_chart(data = chart_df, x = None, y = None, width = 0, height = 0, use_container_width = True)
 
 st.caption(svc.score(test_x_vector, test_y))
 
